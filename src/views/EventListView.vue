fbe778e48cedca551ce7995e91e0795fa0016a13
Yuri Lopes <devyurilopes@gmail.com>
2023-01-14 12:57:16 +0000

    poucas alterações

---
+++ src/views/EventListView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const events = ref(null)
+
+onMounted(() => {
+  
+})
+
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
